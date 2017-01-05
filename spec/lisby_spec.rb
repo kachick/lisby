ee0238102a5c67a688b26e73fed437c237f40d1c
Kenichi Kamiya <kachick1@gmail.com>
2017-01-06 02:19:19 +0900

    Prefer let! in spec fixtures

--- spec/lisby_spec.rb
+++ spec/lisby_spec.rb
@@ -3,23 +3,23 @@
 require_relative 'spec_helper'
 
 describe Lisby do
-  before :each do
-    @lisby = Lisby.new
-    @program = "(begin (define r 3) (* 3.141592653 (* r r)))"
-    @tokens = ["(", "begin", "(", "define", "r", "3", ")", "(", "*", "3.141592653", "(", "*", "r", "r", ")", ")", ")"]
-    @parsed = [:begin, [:define, :r, 3], [:'*', 3.141592653, [:'*', :r, :r]]]
-    @result = 28.274333877
-  end
-  
-  describe '#tokenize' do
-    it { expect(@lisby.tokenize @program).to eq(@tokens) }
-  end
-  
-  describe '#parse' do
-    it { expect(@lisby.parse(@program)).to eq(@parsed) }
-  end
-  
-  describe '#evaluate' do
-    it { expect(@lisby.evaluate @parsed).to eq(@result) }
+  let!(:lisby) { Lisby.new }
+  let!(:expression) { "(begin (define r 3) (* 3.141592653 (* r r)))" }
+  let!(:tokens) { ["(", "begin", "(", "define", "r", "3", ")", "(", "*", "3.141592653", "(", "*", "r", "r", ")", ")", ")"] }
+  let!(:parsed) { [:begin, [:define, :r, 3], [:'*', 3.141592653, [:'*', :r, :r]]] }
+  let!(:result) { 28.274333877 }
+
+  context 'basic methods' do
+    describe '#tokenize' do
+      it { expect(lisby.tokenize expression).to eq(tokens) }
+    end
+    
+    describe '#parse' do
+      it { expect(lisby.parse(expression)).to eq(parsed) }
+    end
+    
+    describe '#evaluate' do
+      it { expect(lisby.evaluate parsed).to eq(result) }
+    end
   end
 end
